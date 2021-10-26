18392143a888544be68efdb179c2cba02962287c
David Andersson <david.andersson@zocom.se>
2021-10-26 15:08:19 +0200

    Static folder

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,22 @@ const PORT = process.env.PORT || 1337
 // Middleware - TODO
 // Exempel: static folders, logger, CORS
 app.use(cors())
+app.use( express.static(__dirname + '/build') )
 
 // Endpoints
 // app.use om vi har en separat router-fil
-app.get('/', (req, res) => {
-	console.log('Server received GET request /');
-	res.status(200).send('Server is online')
-})
+// app.get('/', (req, res) => {
+// 	console.log('Server received GET request /');
+// 	res.status(200).send('Server is online')
+// })
 const animalsData = ['Hund', 'Katt', 'Grävling', 'Orm', 'Älg']
 app.get('/animals', (req, res) => {
 	res.send(animalsData)
+})
+
+// Behövs om man använder React Router
+app.get('*', (req, res) => {
+	res.send(__dirname + '/build/index.html')
 })
 
 app.listen(PORT, () => {
